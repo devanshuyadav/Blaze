ecd21501b5293f7654438fd99c888eab59d2d5c2
Devanshu Yadav <devanshuyadav1611@gmail.com>
2023-10-05 15:10:48 +0530

    Update index.js

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -10,9 +10,9 @@ export default function Home() {
             className={`flex min-h-screen flex-col items-center align-middle justify-center p-2 ${inter.className}`}
         >
             <div className="text-6xl p-6 font-bold flex flex-col">
-                <div>Welcome to</div>
+                <div className="text-5xl">Welcome to</div>
                 <div className="text-center">
-                    Sheet<span className="text-green-400">Sync</span>
+                    <span className="text-6xl text-green-400">⚡Blaze</span>
                 </div>
             </div>
             <FileUpload />
